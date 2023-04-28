accd32c4a8138c881ee5c658e918046085192d8e
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-04-28 16:00:24 -0400

    add .github with codeowners & auto-deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ahalabs.dev',
+  base: '/soroban-abundance-token',
+})
