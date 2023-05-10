1b0ae6de0f5cee35a4b803601e1770d12a7febbc
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-05-10 15:09:13 -0400

    feat: include sourcemaps in build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://ahalabs.dev',
   base: '/soroban-abundance-token',
+  vite: {
+    sourcemap: true,
+  },
 })
