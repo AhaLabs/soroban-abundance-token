66650f971a1aabb67a9beb67adbd6191de4b5c0c
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-05-16 09:29:52 -0400

    fix: remove flash of unstyled page at first render
    - always display explanation & token symbol
    - don't display "gettingStarted" or "allReady" at first load

--- src/js/render.ts
+++ src/js/render.ts
@@ -2,33 +2,37 @@ import { fill, hide, show } from './domHelpers'
 import { symbol, balance, decimals } from 'abundance-token'
 import { formatBalance } from './utils'
 
+let tokenSymbol: string
+
 /**
  * update the html based on user & data state
  */
 export default async function render() {
-  if (window.hasFreighter) {
-    document.querySelector('#getFreighter')!.className = 'done'
-  }
-  if (window.sorobanUserAddress) {
-    document.querySelector('#enableFreighter')!.className = 'done'
-  }
-  if (window.freighterNetwork?.network === 'Futurenet') {
-    document.querySelector('#selectFuturenet')!.className = 'done'
+  if (!tokenSymbol) {
+    tokenSymbol = await symbol()
+    fill('tokenSymbol').with(tokenSymbol)
   }
 
-  const readyToGo = window.hasFreighter && window.sorobanUserAddress
-
-  if (readyToGo) {
-    fill('tokenSymbol').with(await symbol())
+  if (window.hasFreighter && window.sorobanUserAddress) {
+    hide('gettingStarted')
 
     fill('tokenBalance').with(formatBalance(
       await balance({ id: window.sorobanUserAddress! }),
       await decimals()
     ))
 
-    hide('gettingStarted')
     show('allReady')
   } else {
+    if (window.hasFreighter) {
+      document.querySelector('#getFreighter')!.className = 'done'
+    }
+    if (window.sorobanUserAddress) {
+      document.querySelector('#enableFreighter')!.className = 'done'
+    }
+    if (window.freighterNetwork?.network === 'FUTURENET') {
+      document.querySelector('#selectFuturenet')!.className = 'done'
+    }
+
     hide('allReady')
     show('gettingStarted')
   }
