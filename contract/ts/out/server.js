d2a91bf02da56008cbf8d979b63e20a43ec8b67c
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-05-16 14:21:36 -0400

    docs: explain Server

--- contract/ts/out/server.js
+++ contract/ts/out/server.js
@@ -1,3 +1,7 @@
 import * as SorobanClient from 'soroban-client';
 import { RPC_URL } from './constants';
+/**
+ * SorobanClient.Server instance, initialized using {@link RPC_URL} used to
+ * initialize this library.
+ */
 export const Server = new SorobanClient.Server(RPC_URL);
