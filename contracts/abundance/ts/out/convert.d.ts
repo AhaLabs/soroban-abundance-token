2f95c17a4aa09a69f7218ac0bcb5d67d4bc38766
Willem Wyndham <willem@ahalabs.dev>
2023-05-22 11:19:31 -0400

    chore: remove built library

--- contracts/abundance/ts/out/convert.d.ts
+++
@@ -1,5 +0,0 @@
-import * as SorobanClient from 'soroban-client';
-export declare function scvalToBigInt(scval: SorobanClient.xdr.ScVal | undefined): BigInt;
-export declare function scValToJs<T>(base64_xdr: string): T;
-export declare function xdrUint64ToNumber(value: SorobanClient.xdr.Uint64): number;
-export declare function scvalToString(value: SorobanClient.xdr.ScVal): string | undefined;
